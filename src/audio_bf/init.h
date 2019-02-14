88dfe4f37a28ced804cc75f5ceece7314d33ff6a
0x00-pl <0x00.pl@gmail.com>
2019-02-14 14:11:24 +0000

    fix dma function calls

--- src/audio_bf/init.h
+++ src/audio_bf/init.h
@@ -8,106 +8,70 @@
 #include "gpiohs.h"
 
 
-#ifndef AUDIO_BF_DIR_ENABLE
-#define AUDIO_BF_DIR_ENABLE 1
+#define DIRECTION_RES 16
+#define I2S_FS 44100
+#define SOUND_SPEED 340
+#define M_PI       3.14159265358979323846
+
+#ifndef APU_DIR_ENABLE
+#define APU_DIR_ENABLE 1
 #endif
 
-#ifndef AUDIO_BF_VOC_ENABLE
-#define AUDIO_BF_VOC_ENABLE 1
+#ifndef APU_VOC_ENABLE
+#define APU_VOC_ENABLE 1
 #endif
 
-#ifndef AUDIO_BF_DMA_ENABLE
-#define AUDIO_BF_DMA_ENABLE 0
+#ifndef APU_DMA_ENABLE
+#define APU_DMA_ENABLE 1
 #endif
 
-#ifndef AUDIO_BF_FFT_ENABLE
-#define AUDIO_BF_FFT_ENABLE 0
+#ifndef APU_FFT_ENABLE
+#define APU_FFT_ENABLE 0
 #endif
 
-#ifndef AUDIO_BF_DATA_DEBUG
-#define AUDIO_BF_DATA_DEBUG 0
+#ifndef APU_SMPL_SHIFT
+#define APU_SMPL_SHIFT 0x00
 #endif
 
-#ifndef AUDIO_BF_GAIN_DEBUG
-#define AUDIO_BF_GAIN_DEBUG 0
+#ifndef APU_SATURATION_DEBUG
+#define APU_SATURATION_DEBUG 0
 #endif
 
-#ifndef AUDIO_BF_SETDIR_DEBUG
-#define AUDIO_BF_SETDIR_DEBUG 0
+#ifndef APU_SATURATION_VPOS_DEBUG
+#define APU_SATURATION_VPOS_DEBUG 0x07ff
 #endif
 
-#ifndef AUDIO_BF_SMPL_SHIFT
-#define AUDIO_BF_SMPL_SHIFT 0x00
+#ifndef APU_SATURATION_VNEG_DEBUG
+#define APU_SATURATION_VNEG_DEBUG 0xf800
 #endif
 
-#ifndef AUDIO_BF_SATURATION_DEBUG
-#define AUDIO_BF_SATURATION_DEBUG 0
+#ifndef APU_AUDIO_GAIN_TEST
+#define APU_AUDIO_GAIN_TEST (1 << 10)
 #endif
 
-#ifndef AUDIO_BF_SATURATION_VPOS_DEBUG
-#define AUDIO_BF_SATURATION_VPOS_DEBUG 0x07ff
-#endif
+#define APU_DIR_DMA_CHANNEL DMAC_CHANNEL3
+#define APU_VOC_DMA_CHANNEL DMAC_CHANNEL4
 
-#ifndef AUDIO_BF_SATURATION_VNEG_DEBUG
-#define AUDIO_BF_SATURATION_VNEG_DEBUG 0xf800
-#endif
+#define APU_DIR_CHANNEL_MAX 16
+#define APU_DIR_CHANNEL_SIZE 512
+#define APU_VOC_CHANNEL_SIZE 512
 
-#ifndef AUDIO_BF_INPUT_CONST_DEBUG
-#define AUDIO_BF_INPUT_CONST_DEBUG 0x0
-#endif
-
-#ifndef AUDIO_BF_SMPL_SHIFT_DEBUG
-#define AUDIO_BF_SMPL_SHIFT_DEBUG 0
-#endif
-
-#ifndef I2S_RESOLUTION_TEST
-#define I2S_RESOLUTION_TEST RESOLUTION_12_BIT
-#endif
-
-#ifndef I2S_SCLK_CYCLES_TEST
-#define I2S_SCLK_CYCLES_TEST SCLK_CYCLES_16
-#endif
-
-#ifndef SYSCTL_THRESHOLD_I2S0_TEST
-#define SYSCTL_THRESHOLD_I2S0_TEST 0xf
-#endif
-
-#ifndef AUDIO_BF_AUDIO_GAIN_TEST
-#define AUDIO_BF_AUDIO_GAIN_TEST (1 << 10)
-#endif
-
-#ifndef AUDIO_BF_PRESETN_DEBUG
-#define AUDIO_BF_PRESETN_DEBUG 1
-#endif
-
-#ifndef AUDIO_BF_DEBUG_NO_EXIT
-#define AUDIO_BF_DEBUG_NO_EXIT 1
-#endif
-
-
-#define AUDIO_BF_DIR_DMA_CHANNEL DMAC_CHANNEL3
-#define AUDIO_BF_VOC_DMA_CHANNEL DMAC_CHANNEL4
-
-#define AUDIO_BF_DIR_CHANNEL_MAX 16
-#define AUDIO_BF_DIR_CHANNEL_SIZE 512
-#define AUDIO_BF_VOC_CHANNEL_SIZE 512
-
-#if AUDIO_BF_FFT_ENABLE
-extern uint32_t AUDIO_BF_DIR_FFT_BUFFER[AUDIO_BF_DIR_CHANNEL_MAX]
-				       [AUDIO_BF_DIR_CHANNEL_SIZE]
+#if APU_FFT_ENABLE
+extern volatile uint32_t APU_DIR_FFT_BUFFER[APU_DIR_CHANNEL_MAX]
+				       [APU_DIR_CHANNEL_SIZE]
 	__attribute__((aligned(128)));
-extern uint32_t AUDIO_BF_VOC_FFT_BUFFER[AUDIO_BF_VOC_CHANNEL_SIZE]
+extern volatile uint32_t APU_VOC_FFT_BUFFER[APU_VOC_CHANNEL_SIZE]
 	__attribute__((aligned(128)));
 #else
-extern int16_t AUDIO_BF_DIR_BUFFER[AUDIO_BF_DIR_CHANNEL_MAX]
-				  [AUDIO_BF_DIR_CHANNEL_SIZE]
+extern volatile int16_t APU_DIR_BUFFER[APU_DIR_CHANNEL_MAX]
+				  [APU_DIR_CHANNEL_SIZE]
 	__attribute__((aligned(128)));
-extern int16_t AUDIO_BF_VOC_BUFFER[AUDIO_BF_VOC_CHANNEL_SIZE]
+extern volatile int16_t APU_VOC_BUFFER[APU_VOC_CHANNEL_SIZE]
 	__attribute__((aligned(128)));
 #endif
 
 
-extern uint64_t dir_logic_count;
-extern uint64_t voc_logic_count;
+extern volatile uint64_t dir_logic_count;
+extern volatile uint64_t voc_logic_count;
 
 void init_all(void);
--- src/audio_bf/main.c
+++ src/audio_bf/main.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <printf.h>
-#include "audio_bf.h"
+#include "apu.h"
 
 int count;
 int assert_state;
@@ -15,242 +15,33 @@ int dir_logic(void)
 	int32_t dir_max = 0;
 	uint16_t contex = 0;
 
-	for (size_t ch = 0; ch < AUDIO_BF_DIR_CHANNEL_MAX; ch++) { //
+#if APU_FFT_ENABLE
+#else
+	for (size_t ch = 0; ch < APU_DIR_CHANNEL_MAX; ch++) { //
 
-			for (size_t i = 0; i < AUDIO_BF_DIR_CHANNEL_SIZE; i++) { //
-				 dir_sum += (int32_t)AUDIO_BF_DIR_BUFFER[ch][i] * (int32_t)AUDIO_BF_DIR_BUFFER[ch][i];
+			for (size_t i = 0; i < APU_DIR_CHANNEL_SIZE; i++) { //
+				 dir_sum += (int32_t)APU_DIR_BUFFER[ch][i] * (int32_t)APU_DIR_BUFFER[ch][i];
 			}
-			dir_sum = dir_sum / AUDIO_BF_DIR_CHANNEL_SIZE;
+			dir_sum = dir_sum / APU_DIR_CHANNEL_SIZE;
 			if(dir_sum > dir_max){
 				dir_max = dir_sum;
 				contex = ch;
 			}
-			printf("%d	", dir_sum);
+			//printf("%d	", dir_sum);
 		}
 		printf("   %d\n", contex);
 		printf("\n");
-
-#if 0
-	if (count < 5) {
-#if AUDIO_BF_DATA_DEBUG
-#if AUDIO_BF_FFT_ENABLE
-		for (size_t i = 0; i < AUDIO_BF_DIR_CHANNEL_SIZE; i++) { //
-			printk("[%lu]%08x ", i, AUDIO_BF_DIR_FFT_BUFFER[1][i]);
-		}
-		printk("\n");
-		printk("\n");
-#else
-		for (size_t ch = 1; ch < AUDIO_BF_DIR_CHANNEL_MAX; ch++) { //
-
-			for (size_t i = 1; i < AUDIO_BF_DIR_CHANNEL_SIZE; i++) { //
-				if (AUDIO_BF_DIR_BUFFER[ch][i] != AUDIO_BF_DIR_BUFFER[ch][i - 1] + 1) { //
-					printk("dir at %lx %lx, expect inc %x got %x\n",
-					       ch, i,
-					       (AUDIO_BF_DIR_BUFFER[ch][i - 1] + 1) & 0xffff,
-					       AUDIO_BF_DIR_BUFFER[ch][i] & 0xffff);
-					assert_state = -1;
-				}
-				if (AUDIO_BF_DIR_BUFFER[ch][i] != AUDIO_BF_DIR_BUFFER[ch - 1][i] + 1) { //
-					printk("dir at %lx %lx, expect offset-prev %x got %x\n",
-					       ch, i,
-					       (AUDIO_BF_DIR_BUFFER[ch - 1][i] + 1) & 0xffff,
-					       AUDIO_BF_DIR_BUFFER[ch][i] & 0xffff);
-					assert_state = -1;
-				}
-			}
-		}
 #endif
-#endif
-#if AUDIO_BF_SMPL_SHIFT_DEBUG
-		uint32_t smpl_shift = audio_bf_get_smpl_shift();
 
-		for (size_t ch = 1; ch < AUDIO_BF_DIR_CHANNEL_MAX; ch++) { //
-			for (size_t i = 1; i < AUDIO_BF_DIR_CHANNEL_SIZE;
-			     i++) { //
-				int16_t expt = 1 << smpl_shift;
-				int16_t current = AUDIO_BF_DIR_BUFFER[ch][i];
-
-				if (current != expt) {
-					printk("smpl_shift check dir at %lx %lx, expect %x got %x\n",
-					       ch, i, expt, current);
-					assert_state = -1;
-				}
-			}
-		}
-
-		audio_bf_set_smpl_shift(smpl_shift + 1);
-#endif
-	//	audio_bf_voc_set_direction(count % 12);
-	//	audio_bf_dir_enable();
-	//	count++;
-	}
-	#endif	
-
-
-	audio_bf_dir_enable();
+	apu_dir_enable();
 	return 0;
 }
 
 int voc_logic(void)
 {
-	#if 0
-//	printk("%s\n", __func__);
-	if (count < 5) {
-#if AUDIO_BF_DATA_DEBUG & !(AUDIO_BF_GAIN_DEBUG)
-#if AUDIO_BF_FFT_ENABLE
-		for (size_t i = 0; i < AUDIO_BF_DIR_CHANNEL_SIZE; i++) { //
-			printk("[%lu]%08x ", i, AUDIO_BF_VOC_FFT_BUFFER[i]);
-		}
-		printk("\n");
-		printk("\n");
-#else
-		for (size_t i = 1; i < AUDIO_BF_VOC_CHANNEL_SIZE; i++) { //
-			if (AUDIO_BF_VOC_BUFFER[i]
-			    != AUDIO_BF_VOC_BUFFER[i - 1] + 1) { //
-				printk("voc at %lx, expect %x got %x\n", i,
-				       AUDIO_BF_VOC_BUFFER[i - 1] + 1,
-				       (uint32_t)AUDIO_BF_VOC_BUFFER[i]
-					       & 0xffff);
-				assert_state = -1;
-			}
-		}
-#endif
-#endif
-#if AUDIO_BF_GAIN_DEBUG
-		for (size_t i = 1; i < AUDIO_BF_VOC_CHANNEL_SIZE; i++) { //
-			if (AUDIO_BF_VOC_BUFFER[i]
-			    != AUDIO_BF_VOC_BUFFER[i - 1]) {
-				if (AUDIO_BF_VOC_BUFFER[i]
-				    == AUDIO_BF_VOC_BUFFER[i - 1] << 1) {
-					; // pass
-				} else if (AUDIO_BF_VOC_BUFFER[i] <= 1) {
-					; // pass
-				} else {  //
-					printk("voc not expect %d %d\n",
-					       AUDIO_BF_VOC_BUFFER[i],
-					       AUDIO_BF_VOC_BUFFER[i - 1]);
-					assert_state = -1;
-				}
-			}
-		}
-#endif
-#if AUDIO_BF_SMPL_SHIFT_DEBUG
-		uint32_t smpl_shift = audio_bf_get_smpl_shift();
-
-		for (size_t i = 1; i < AUDIO_BF_VOC_CHANNEL_SIZE; i++) { //
-			int16_t expt = 1 << smpl_shift;
-			int16_t current = AUDIO_BF_VOC_BUFFER[i];
-
-			if (current != expt) { //
-				printk("smpl_shift check voc at %lx, expect %x got %x\n",
-				       i, expt, current);
-				assert_state = -1;
-			}
-		}
-
-		audio_bf_set_smpl_shift(smpl_shift + 1);
-#endif
-#if AUDIO_BF_SATURATION_DEBUG
-		uint32_t counters = audio_bf_voc_get_saturation_counter();
-
-		audio_bf_voc_reset_saturation_counter();
-
-		uint16_t cnt_u = counters >> 16;
-		uint16_t cnt_d = counters & 0xffff;
-
-		if (AUDIO_BF_INPUT_CONST_DEBUG >= AUDIO_BF_SATURATION_VPOS_DEBUG
-		    || AUDIO_BF_INPUT_CONST_DEBUG
-			       <= AUDIO_BF_SATURATION_VNEG_DEBUG) {
-			if (cnt_u != cnt_d) { //
-				printk("saturation check voc warning: %d / %d\n",
-				       cnt_u, cnt_d);
-			}
-		} else {
-			if (cnt_u != 0) { //
-				printk("saturation check voc warning: %d / %d\n",
-				       cnt_u, cnt_d);
-			}
-		}
-#endif
-#if AUDIO_BF_SETDIR_DEBUG
-		struct audio_bf_ch_cfg_t ch_cfg = audio_bf->bf_ch_cfg_reg;
-		uint32_t dir = ch_cfg.bf_target_dir;
-		uint32_t found_7 = 0;
-
-		for (size_t i = 32; i < AUDIO_BF_VOC_CHANNEL_SIZE - 32;
-		     i++) {				   //
-			if (AUDIO_BF_VOC_BUFFER[i] == 7) { //
-				found_7 = 1;
-				if (AUDIO_BF_VOC_BUFFER[i + dir] != 1) { //
-					printk("voc setdir at %lx, got\n %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n",
-					       i, AUDIO_BF_VOC_BUFFER[i - 8],
-					       AUDIO_BF_VOC_BUFFER[i - 7],
-					       AUDIO_BF_VOC_BUFFER[i - 6],
-					       AUDIO_BF_VOC_BUFFER[i - 5],
-					       AUDIO_BF_VOC_BUFFER[i - 4],
-					       AUDIO_BF_VOC_BUFFER[i - 3],
-					       AUDIO_BF_VOC_BUFFER[i - 2],
-					       AUDIO_BF_VOC_BUFFER[i],
-					       AUDIO_BF_VOC_BUFFER[i + 1],
-					       AUDIO_BF_VOC_BUFFER[i + 2],
-					       AUDIO_BF_VOC_BUFFER[i + 3],
-					       AUDIO_BF_VOC_BUFFER[i + 4],
-					       AUDIO_BF_VOC_BUFFER[i + 5],
-					       AUDIO_BF_VOC_BUFFER[i + 6],
-					       AUDIO_BF_VOC_BUFFER[i + 7],
-					       AUDIO_BF_VOC_BUFFER[i + 8]);
-					assert_state = -1;
-				}
-			}
-		}
-		if (!found_7) {
-			printk("voc setdir not found 7, please check input data.\n");
-			assert_state = -1;
-		}
-		dir = dir + 3;
-		audio_bf_voc_set_direction(dir % 16);
-#endif
-		count++;
-	}
-	#endif
 	return 0;
 }
 
-/*#if AUDIO_BF_GAIN_DEBUG
-int set_gain(void)
-{
-	for (size_t i = 0; i < 100; i++) { //
-		int32_t gain = 1 << (i % 11);
-
-		audio_bf_set_audio_gain(gain);
-		struct audio_bf_ch_cfg_t ch_cfg = audio_bf->bf_ch_cfg_reg;
-
-		if (ch_cfg.audio_gain != gain) { //
-			printk("[error]: audio_bf_set_audio_gain failed.\n");
-			assert_state = -1;
-		}
-	}
-	return 0;
-}
-#endif*/
-
-/*#if AUDIO_BF_SETDIR_DEBUG
-int set_direction(void)
-{
-	for (size_t i = 0; i < 100; i++) {
-		int32_t dir = (i % 16);
-
-		audio_bf_voc_set_direction(dir);
-		struct audio_bf_ch_cfg_t ch_cfg = audio_bf->bf_ch_cfg_reg;
-
-		if (ch_cfg.bf_target_dir != dir) { //
-			printk("[error]: audio_bf_voc_set_direction failed.\n");
-			assert_state = -1;
-		}
-	}
-	return 0;
-}
-#endif*/
 
 int event_loop(void)
 {
@@ -284,15 +75,7 @@ int main(void)
 	set_csr(mie, MIP_MEIP);
 	set_csr(mstatus, MSTATUS_MIE);
 
-/*#if AUDIO_BF_GAIN_DEBUG
-	set_gain();
-#endif
-#if AUDIO_BF_SETDIR_DEBUG
-	set_direction();
-#endif
-*/
 	event_loop();
 	
-// #endif
 
 }
