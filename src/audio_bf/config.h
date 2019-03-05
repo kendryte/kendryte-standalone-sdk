c57dcb639086dca194b277a21bad51fa7174be34
0x00-pl <0x00.pl@gmail.com>
2019-03-05 11:37:01 +0000

    good result with fs=44100

--- src/audio_bf/config.h
+++ src/audio_bf/config.h
@@ -4,15 +4,15 @@
 #define DMA_CH_I2S_TX DMAC_CHANNEL1
 #define DMA_CH_FFT_RX DMAC_CHANNEL2
 #define DMA_CH_FFT_TX DMAC_CHANNEL3
-#define SAMPLE_RATE 16000UL
+#define SAMPLE_RATE 44100UL
 
 // aec
 #define AEC_FRAME_LEN  2048UL
-#define AEC_TAIL AEC_FRAME_LEN*6UL  // *7
+#define AEC_TAIL AEC_FRAME_LEN*4UL  // *7
 
 // devererberation
 #define KAL_FRAME_LEN  512UL
 #define KAL_FRAME_SHIFT 512UL  // 256
 #define KAL_NRE 257UL
-#define KAL_WPE_L 14UL  // 16
+#define KAL_WPE_L 6UL  // 16
 #define KAL_ALPHA  0.98f
