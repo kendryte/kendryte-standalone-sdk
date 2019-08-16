9f54db5e4c1372ffca30d6861cfd19fcdb2a8808
sunnycase <sunnycase@live.cn>
2019-08-16 11:17:13 +0800

    Update nncase runtime

--- lib/nncase/include/runtime/neutral/neutral_sim_types.h
+++ lib/nncase/include/runtime/neutral/neutral_sim_types.h
@@ -13,24 +13,17 @@
  * limitations under the License.
  */
 #pragma once
-#include <filesystem>
-#include <fstream>
-#include <vector>
+#include <array>
+#include <cstdint>
+#include <datatypes.h>
+#include <xtensor/xstorage.hpp>
 
 namespace nncase
 {
-inline std::vector<uint8_t> read_file(const std::filesystem::path &filename)
+namespace runtime
 {
-    std::ifstream infile(filename, std::ios::binary | std::ios::in);
-    if (!infile.good())
-        throw std::runtime_error("Cannot open file: " + filename.string());
-
-    infile.seekg(0, std::ios::end);
-    size_t length = infile.tellg();
-    infile.seekg(0, std::ios::beg);
-    std::vector<uint8_t> data(length);
-    infile.read(reinterpret_cast<char *>(data.data()), length);
-    infile.close();
-    return data;
+    namespace neutral
+    {
+    }
 }
 }
