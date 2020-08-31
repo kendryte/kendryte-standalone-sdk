80086e2a90b80f8c84855ffc6fcfa6073fec99e3
noahzhy <noahzhy@gmail.com>
2020-08-31 18:47:56 +0900

    add

--- src/beta/main.c
+++ src/beta/main.c
@@ -1,73 +1,85 @@
 #include "rtc.h"
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include <unistd.h>
 #include "htpa.h"
 #include "sipeed_i2c.h"
 #include "htpa_32x32d.h"
 #include "fpioa.h"
+#include "ff.h"
 
 htpa_t htpa;
 
-void get_date_time(bool alarm) {
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;
+void get_date_time(char *datetime) {
+    int year, month, day, hour, minute, second;
     rtc_timer_get(&year, &month, &day, &hour, &minute, &second);
-    printf("%4d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second);
+    sprintf(datetime, "%4d-%02d-%02d %02d:%02d:%02d", year, month, day, hour,
+            minute, second);
+    printf("%s\n", datetime);
 }
 
 int main(void) {
+    char datetime[19];
+
     rtc_init();
-    get_date_time(false);
-    rtc_timer_set(2020, 8, 29, 14, 0, 0);
-    // rtc_alarm_set(2018, 9, 12, 23, 31, 00);
-    get_date_time(false);
+    rtc_timer_set(2020, 9, 1, 9, 0, 0);
+    get_date_time(datetime);
     printf("start loop\n");
 
-    int32_t pixels[1024];
-    int32_t min_max[4];
-    uint8_t img[1024];
-    uint16_t height;
+    FIL file;
+    FRESULT ret = FR_OK;
+    FILINFO v_fileinfo;
+    uint32_t v_ret_len = 0;
 
-    i2c_device_number_t i2c_num = I2C_DEVICE_0;
-    // // i2c_init(i2c_num);
-    int res = htpa_init(&htpa, i2c_num, 18, 19, 1000000);
-    printf("htpa init status: %d\n", res);
-    // htpa_height(htpa, );
-    if (res == 0) {
-        int re = htpa_temperature(&htpa, pixels);
-        printf("htpa_temperature: %d\n", re);
+    char *path = "attendance.csv";
+    char *added_attend;
+    sprintf(added_attend, "%s, %s, %f, %s\n", datetime, "user1", 36.5, "yes");
+
+    if ((ret = f_stat(path, &v_fileinfo)) == FR_OK) {
+        // file exists
+        printf("%s length is %lld\n", path, v_fileinfo.fsize);
+        ret = f_open(&file, path, FA_OPEN_EXISTING | FA_WRITE);
+        ret = f_lseek(&file, f_size(&file));
+        ret = f_write(&file, added_attend, sizeof(added_attend), &v_ret_len);
+    } else {
+        if ((ret = f_open(&file, path, FA_CREATE_NEW | FA_WRITE)) == FR_OK) {
+            printf("Open %s ok\n", path);
+            // write csv header
+            char csv_header[42] = "timestamp, id, ambient_temperature, mask\n";
+            ret = f_write(&file, csv_header, sizeof(csv_header), &v_ret_len);
+            if (ret != FR_OK) {
+                printf("Write %s err[%d]\n", path, ret);
+            } else {
+                printf("Write %d bytes to %s ok\n", v_ret_len, path);
+            }
+    }
+    f_close(&file);
+    printf("file status: %d\n", ret);
+    // file test success
+
+    int32_t pixels[1024], min_max[4];
+    uint8_t htpa_img[1024];
+
+    int htpa_stat = htpa_init(&htpa, I2C_DEVICE_0, 18, 19, 1000000);
+    printf("htpa init status: %d\n", htpa_stat);
+    if (htpa_stat == 0) {
+        int htpa_temp = htpa_temperature(&htpa, pixels);
+        printf("htpa_temperature: %d\n", htpa_temp);
         for (int i=0; i<1024; i++) {
             printf("%d,", pixels[i]);
         }
         printf("\n");
-    }else {
-        printf("==== init status: %d\n", res);
     }
-
-    // htpa_height(&htpa, &height);
-    // printf("height: %d\n", height);
-    htpa_get_min_max(&htpa, min_max);
-    htpa_get_to_image(&htpa, min_max[0], min_max[1], img);
-
-    // fpioa_set_function(18, FUNC_I2C0_SCLK);
-    // fpioa_set_function(19, FUNC_I2C0_SDA);
-    // maix_i2c_init(i2c_num, 7, HTPA_EEPROM_I2C_FREQ);
-    // uint8_t temp[2] = {HTPA_CMD_ADDR_CONF, 0x01}; // status register
-    // int res = maix_i2c_send_data(i2c_num, HTPA_ADDR, temp, 1, 100);
-    // printf("htpa init status: %d\n", res);
-    // htpa_temperature(&htpa, &pixels);
-    // for (int i=0; i<1024; i++) {
-    //     printf("%d,", pixels[i]);
+    // else {
+    //     printf("==== init status: %d\n", res);
     // }
+    htpa_get_min_max(&htpa, min_max);
+    htpa_get_to_image(&htpa, min_max[0], min_max[1], htpa_img);
 
     while (1) {
         sleep(1);
-        get_date_time(false);
+        get_date_time(datetime);
     }
     return 0;
 }
