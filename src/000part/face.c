9354fcff794b1d50259659534448f825c628c73e
noahzhy <noahzhy@gmail.com>
2020-09-16 04:04:34 +0900

    add

---
+++ src/000part/face.c
@@ -0,0 +1,30 @@
+/* Copyright 2018 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "face.h"
+// #include "iomem.h"
+#include <stdlib.h>
+#include <cmath>
+
+
+double cosine_similarity(double *A, double *B, unsigned int Vector_Length) {
+    double dot = 0.0, denom_a = 0.0, denom_b = 0.0;
+    for (unsigned int i = 0u; i < Vector_Length; ++i) {
+        dot += A[i] * B[i];
+        denom_a += A[i] * A[i];
+        denom_b += B[i] * B[i];
+    }
+    return dot / (sqrt(denom_a) * sqrt(denom_b));
+}
