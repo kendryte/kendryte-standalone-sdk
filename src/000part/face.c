e7630589c9863f336e83ce8c912761d1149ac863
noahzhy <noahzhy@gmail.com>
2020-09-21 18:06:54 +0900

    add update

--- src/000part/face.c
+++ src/000part/face.c
@@ -14,10 +14,13 @@
  */
 
 #include "face.h"
-// #include "iomem.h"
+#include "iomem.h"
 #include <stdlib.h>
 // #include <cmath>
-#include <math>
+#include <math.h>
+#include <string.h>
+
+extern const float xyz_table[256];
 
 static inline int fast_roundf(float x) { return (int)(x); }
 
@@ -35,29 +38,89 @@ void crop_image(uint8_t *src, uint8_t* dst, int x1, int y1, int x2, int y2, int 
     // uint8_t dst[3][(x2-x1) * (y2-y1)];
     // int limit = (x2-x1) * (y2-y1);
     int count = 0;
-    int fixed = 0;
+    int fixed = width*height;
+    uint8_t r, g, b;
+    // uint8_t gray;
+    uint8_t* grayscale = (uint8_t *)iomem_malloc((x2-x1)*(y2-y1));
 
-    for (int i=0; i<3; i++) {
-        count = 0;
-        fixed = width*height*i;
-        for (int j=y1; j<=y2; j++) {
-            for (int k=x1; k<=x2; k++) {
-                dst[i][count] = src[(j-1)*width + k + fixed];
-                count++;
+    for (int j=y1; j<=y2; j++) {
+        for (int k=x1; k<=x2; k++) {
+            r = src[(j-1)*width + k];
+            g = src[(j-1)*width + k + fixed];
+            b = src[(j-1)*width + k + fixed * 2];
+            grayscale[count] = rgb_to_grayscale(r, g, b);
+            // bug here, count max to 96*96
+            // grayscale[count] = gray;
+            count++;
+        }
+    }
+    image_resize(grayscale, x2-x1, y2-y1, dst, 96, 96);
+}
+
+void image_resize(uint8_t* in, int w0, int h0, uint8_t* out, int w, int h) {
+    // image_t *img = (image_t *)py_image_cobj(img_obj);
+    // int w = target_w;
+    // int h = target_h;
+    // src's w, h
+    // int w0 = img->w;
+    // int h0 = img->h;
+    // uint8_t *out = xalloc(w * h);
+    // uint8_t *in = src;
+    float sx = (float)(w0) / w;
+    float sy = (float)(h0) / h;
+    int x, y, x0, y0, x1, y1, val_x1, val_y1;
+    float xf, yf;
+    // mp_obj_t image = py_image(w, h, img->bpp, out);
+    if (w >= w0 || h >= h0) {
+        for (y = 0; y < h; y++) {
+            yf = (y + 0.5) * sy - 0.5;
+            y0 = (int)yf;
+            y1 = y0 + 1;
+            val_y1 = y0 < h0 - 1 ? y1 : y0;
+            for (x = 0; x < w; x++) {
+                xf = (x + 0.5) * sx - 0.5;
+                x0 = (int)xf;
+                x1 = x0 + 1;
+                val_x1 = x0 < w0 - 1 ? x1 : x0;
+                out[y * w + x] = (uint8_t)(
+                    in[y0 * w0 + x0] * (x1 - xf) * (y1 - yf) +
+                    in[y0 * w0 + val_x1] * (xf - x0) * (y1 - yf) +
+                    in[val_y1 * w0 + x0] * (x1 - xf) * (yf - y0) +
+                    in[val_y1 * w0 + val_x1] * (xf - x0) * (yf - y0));
+            }
+        }
+    } else {
+        for (y = 0; y < h; y++) {
+            y0 = y * sy;
+            y1 = (y + 1) * sy;
+            for (x = 0; x < w; x++) {
+                x0 = x * sy;
+                x1 = (x + 1) * sy;
+                int sum, xx, yy;
+                sum = 0;
+                for (yy = y0; yy <= y1; yy++) {
+                    for (xx = x0; xx <= x1; xx++) {
+                        sum += in[yy * w0 + xx];
+                    }
+                }
+                out[y * w + x] =
+                    sum / ((y1 - y0 + 1) *
+                            (x1 - x0 + 1)); // avg to get better picture
             }
         }
     }
+    // return image;
 }
 
-void rgb_to_grayscale(uint8_t *rgb, uint8_t *grayscale) {
-    float r_lin = xyz_table[rgb[0]];
-    float g_lin = xyz_table[rgb[1]];
-    float b_lin = xyz_table[rgb[2]];
+uint8_t rgb_to_grayscale(uint8_t r, uint8_t g, uint8_t b) {
+    float r_lin = xyz_table[r];
+    float g_lin = xyz_table[g];
+    float b_lin = xyz_table[b];
     float y =
         ((r_lin * 0.2126f) + (g_lin * 0.7152f) + (b_lin * 0.0722f)) / 100.0f;
     y = (y > 0.0031308f) ? ((1.055f * powf(y, 0.416666f)) - 0.055f)
                          : (y * 12.92f);
-    grayscale = IM_MAX(IM_MIN(fast_roundf(y * 255), 255), 0);
+    return IM_MAX(IM_MIN(fast_roundf(y * 255), 255), 0);
 }
 
 static void svd22(const float a[4], float u[4], float s[2], float v[4]) {
